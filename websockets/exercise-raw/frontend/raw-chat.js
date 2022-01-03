966e8910dbedc4818b98f2976525777ea4a216b1
Johan Arcos <jarco0204@outlook.com>
2022-01-03 12:44:08 -0330

    websocket written by hand

--- websockets/exercise-raw/frontend/raw-chat.js
+++ websockets/exercise-raw/frontend/raw-chat.js
@@ -11,14 +11,26 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  // code goes here
+  const data = { user, text };
+  ws.send(JSON.stringify(data));
 }
 
-/*
- *
- * your code goes here
- *
- */
+// Handles entire handshake
+const ws = new WebSocket("ws://localhost:8080", ["json"]);
+
+ws.addEventListener("open", () => {
+  console.log("connected");
+  presence.innerText = "💚";
+});
+ws.addEventListener("close", () => {
+  presence.innerText = "😍";
+});
+
+ws.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data);
+  allChat = data.msg;
+  render();
+});
 
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
